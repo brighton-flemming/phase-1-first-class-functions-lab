f805c0a4bc24d2f1ca7582e29a264de2c2e23b0c
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-06-17 16:43:04 +0300

    Mission Passed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers( drivers){
+  return drivers.slice(0, 2)
+}
+function  returnLastTwoDrivers (drivers){
+    return drivers.slice(2,4) ;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] ;
+
+function createFareMultiplier(numMultiply){
+   return (num) => num * numMultiply ;
+}
+
+let fareDoubler =  createFareMultiplier(2) ;
+
+let fareTripler = createFareMultiplier(3) ;
+
+function selectDifferentDrivers (drivers, returnDrivers){
+  return returnDrivers(drivers) ;
+}
+    
